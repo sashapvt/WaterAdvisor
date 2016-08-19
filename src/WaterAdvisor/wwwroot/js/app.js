2bad48224527eef0a9842905a54d3f6064edc42a
sashapvt <sashapvt@gmail.com>
2016-08-19 16:57:38 +0300

    В тестовому контролері реалізована функція оновлення даних з серверу через ApiControler

--- src/WaterAdvisor/wwwroot/js/app.js
+++ src/WaterAdvisor/wwwroot/js/app.js
@@ -1,49 +1,21 @@
-﻿// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
-function ___AppViewModel() {
-    this.Id = ko.observable(document.getElementById('Id').value);
-    this.ProjectName = ko.observable();
-    this.ProjectComment = ko.observable();
-    this.Date = ko.observable();
-    this.Project = ko.observable();
-    var data;
-    $.get('/Api/Get', { id: this.Id }, function (data) { alert(data.projectName) }, 'json');
-    //alert(data);
-    //this.ProjectName(this.Project.ProjectName);
-}
+﻿var Id = document.getElementById('Id').value;
+var AppViewModel;
+
+GetData();
 
 function GetData() {
-    var Id = document.getElementById('Id').value;
-    var result;
-    $.get('/Api/Get', { id: Id }, result = function (data) { alert(data.projectName); return data; }, 'json');
-    console.log(result());
-    //alert(result);
-    //return data;
+    $.get('/Api/Get', { id: Id }, function (data) {
+        if (AppViewModel == null) {
+            AppViewModel = ko.mapping.fromJS(data);
+            ko.applyBindings(AppViewModel);
+        }
+        else {
+            ko.mapping.fromJS(data, AppViewModel);
+        }
+    }, 'json');
 }
 
-var AppViewModel;
-var Id = document.getElementById('Id').value;
-$.get('/Api/Get', { id: Id }, function (data) { AppViewModel = ko.mapping.fromJS(data); ko.applyBindings(AppViewModel); alert(AppViewModel()); }, 'json');
-//data = GetData();
 
-var viewModel;
-
-// result could look like this: "{ "personId": 1, "firstName": "Marco", "lastName": "Franssen", "age": 26, "webpage": "http://marcofranssen.nl", "twitter": "@marcofranssen" }"$.ajax({
-$.ajax({
-    url: '/Api/Get/' + Id,
-    type: 'GET',
-    dataType: 'JSON',
-    success: function (result) {
-        var data = JSON.parse(result);
-        viewModel = ko.mapping.fromJS(data);
-        ko.applyBindings(viewModel);
-        alert(viewModel);
-    },
-    error: function (result) {
-        //handle the error, left for brevity
-    }
+$("#button_from_server").click(function () {
+    GetData();
 });
-
-//var viewModel = ko.mapping.fromJS(data);
-
-// Activates knockout.js
-//ko.applyBindings(new AppViewModel());
