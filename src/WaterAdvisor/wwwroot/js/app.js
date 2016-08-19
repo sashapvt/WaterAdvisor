81ee350b551fa1e8ed409a7d7897a19cc2a01905
sashapvt <sashapvt@gmail.com>
2016-08-19 17:38:54 +0300

    Почата робота над реалізацією відправки даних на сервер.

--- src/WaterAdvisor/wwwroot/js/app.js
+++ src/WaterAdvisor/wwwroot/js/app.js
@@ -15,7 +15,17 @@ function GetData() {
     }, 'json');
 }
 
+// TODO: Розібрати помилку сервера у прийнятті даних. Додати обробку помилок з сервера? Взяти інший приклад даних? Підняти питання безпеки.
+function PostData() {
+    $.post('/Api/Post', ko.mapping.toJS(AppViewModel), function (data) {
+        alert(data);
+    }, 'json');
+}
 
 $("#button_from_server").click(function () {
     GetData();
+});
+
+$("#button_to_server").click(function () {
+    PostData();
 });
