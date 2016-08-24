a520aaecba3ec0293a629e71cffaed8d5f52924d
Sasha <sashapvt@gmail.com>
2016-08-24 13:36:46 +0300

    Реалізовано в тестовому контролері варіант двостороннього зчитування/оновлення даних на сервері.

--- src/WaterAdvisor/wwwroot/js/app.js
+++ src/WaterAdvisor/wwwroot/js/app.js
@@ -4,20 +4,32 @@ var AppViewModel;
 GetData();
 
 function GetData() {
-    $.get('/Api/Get', { id: Id }, function (data) {
-        if (AppViewModel == null) {
-            AppViewModel = ko.mapping.fromJS(data);
-            ko.applyBindings(AppViewModel);
-        }
-        else {
-            ko.mapping.fromJS(data, AppViewModel);
-        }
-    }, 'json');
+    $.ajax({
+        url: '/Api/Get/'+ Id,
+        cache: false, 
+        success: function (data) {
+            console.log('Get request done');
+            console.log(data);
+            if (AppViewModel == null) {
+                AppViewModel = ko.mapping.fromJS(data);
+                ko.applyBindings(AppViewModel);
+            }
+            else {
+                ko.mapping.fromJS(data, AppViewModel);
+            }
+        },
+        contentType: 'application/json'
+    });
 }
 
 function PostData() {
     var SendData = ko.mapping.toJSON(AppViewModel);
-    $.ajax({ url: '/Api/Post', type: 'post', data: SendData, contentType: 'application/json', success: function (data) { console.log('Post request result: ' + data); } });
+    $.ajax({
+        url: '/Api/Post', type: 'post', data: SendData, contentType: 'application/json', success: function (data) {
+            console.log('Post request result: ' + data);
+            GetData();
+        }
+    });
 }
 
 $("#button_from_server").click(function () {
@@ -25,5 +37,9 @@ $("#button_from_server").click(function () {
 });
 
 $("#button_to_server").click(function () {
+    PostData();
+});
+
+$(':input').change(function () {
     PostData();
 });
