61a2f06c9d8d5e7e1cf233bb857990cb6e562bbe
Sasha <sashapvt@gmail.com>
2016-08-30 21:49:19 +0300

    Почата привязка моделі HomeViewModel

--- src/WaterAdvisor/Controllers/HomeController.cs
+++ src/WaterAdvisor/Controllers/HomeController.cs
@@ -3,33 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WaterAdvisor.Data;
+using WaterAdvisor.Models;
 using WaterAdvisor.Models.Project;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace WaterAdvisor.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ApplicationDbContext context)
+        public HomeController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
-        public IActionResult Index(int? Id)
+        public async Task<IActionResult> Index(int? id)
         {
-            // Todo: Fix cookies, maybe routing
-            if (Id != null)
+            if (id == null)
+            {
+                if (Request.Cookies["lastProjectOpenedId"] == null)
+                    return Redirect("/Project");
+                else
+                    return Redirect("/?id=" + Request.Cookies["lastProjectOpenedId"]);
+            }
+            else
             {
                 if (Request.Cookies["lastProjectOpenedId"] != null) Response.Cookies.Delete("lastProjectOpenedId");
-                Response.Cookies.Append("lastProjectOpenedId", Id.ToString());
-                return View(new HomeViewModel());
+                Response.Cookies.Append("lastProjectOpenedId",id.ToString());
+
+                var homeViewModel = new HomeViewModel();
+                var project = await _context.Project.SingleOrDefaultAsync(m => m.Id == id);
+                if (project == null)
+                {
+                    return NotFound();
+                }
+
+                var currentUserId = _userManager.FindByNameAsync(User.Identity.Name).Result.Id;
+                if (project.UserId != currentUserId)
+                {
+                    return Unauthorized();
+                }
+
+                MapProject(homeViewModel, project);
+
+                return View(homeViewModel);
             }
-            if (Request.Cookies["lastProjectOpenedId"] == null)
-                return Redirect("/Project");
-            else
-                return Redirect("Home/Index/" + Request.Cookies["lastProjectOpenedId"]);
         }
 
         // GET: /Home/Error
@@ -56,6 +81,20 @@ namespace WaterAdvisor.Controllers
             ViewBag.HttpCode = HttpCode;
             ViewBag.Message = message;
             return View();
+        }
+
+        // Helpers
+
+        // Map project
+        private void MapProject(HomeViewModel model, Project project)
+        {
+            model.Id = project.Id;
+            model.ProjectComment = project.ProjectComment;
+            model.ProjectDate = project.ProjectDate;
+            model.ProjectName = project.ProjectName;
+            // TODO: Get waterlist
+            model.WaterIn = new WaterList();
+            model.WaterIn.NH4.Value = 1;
         }
     }
 }
