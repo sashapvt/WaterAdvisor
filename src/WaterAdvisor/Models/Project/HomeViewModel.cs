e3009c71e59f63a9ae311101d87c6af6c41ad379
sashapvt <sashapvt@gmail.com>
2016-09-15 17:08:09 +0300

    В інтерфейсі додані можливості редагування рекавері і коригування рН

--- src/WaterAdvisor/Models/Project/HomeViewModel.cs
+++ src/WaterAdvisor/Models/Project/HomeViewModel.cs
@@ -13,10 +13,20 @@ namespace WaterAdvisor.Models.Project
             WaterIn = new WaterList();
             Calc = new Calc(WaterIn);
         }
+
+        // Project correction data
+        public double RecoveryRO = 75; // RO recovery in % for concentrate parameters calculation
+        public EnumpHCorrection pHCorrection = EnumpHCorrection.None; // 0 - none, 1 - HCl, 2 - H2SO4
+        public double pHCorrected = 0; // Desired pH
+        public double pHCorrectionAcidDose = 0; // Acid dose for pH correction, mg/l
+
         // Input water data
         public WaterList WaterIn { get; set; }
 
         // Calculation
         public Calc Calc { get; set; }
+
+        // Enum for determine pHCorrection
+        public enum EnumpHCorrection : int { None, HCl, H2SO4 }; // 0 - none, 1 - HCl, 2 - H2SO4
     }
 }
